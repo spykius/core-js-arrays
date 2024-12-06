b584da3bf68fc885ecca73cadea0c46ae008eb45
Nikita Korp <betaquarius203@gmail.com>
2024-12-06 22:17:14 +0300

    feat: implement getAverage function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -122,8 +122,9 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr || arr.length === 0) return 0;
+  return +(arr.reduce((start, val) => start + val, 0) / arr.length).toFixed(2);
 }
 
 /**
