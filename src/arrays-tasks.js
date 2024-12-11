5c5b3e3181f0142dc87054ee3b7ffd8445ca2f77
Nikita Korp <betaquarius203@gmail.com>
2024-12-11 10:17:58 +0300

    feat: implement getIndicesOfOddNumbers function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -422,8 +422,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce(
+    (odd, el, i) => (el % 2 === 1 ? odd.concat(i) : odd),
+    []
+  );
 }
 
 /**
